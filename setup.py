5aefb9de69f69d3d1077a2c3922f2bb28f2e8d2a
John R. Eakin <johnreakin@gmail.com>
2022-12-17 22:08:58 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jay_txlege',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='Texas Legislature Member & Bill Parsing'
+)
